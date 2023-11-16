c53a2f4307ea5ea378315dce98990c35217fa3a9
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-11-15 22:22:45 -0800

    Seed database, create test route to query users

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+new_user = User.create ({first_name: "Bon", last_name: "Jovi", phone_number: "23456789",home_address: "111 Bridgeport Avenue", email_address: "a@test.com", user_request: "window washing"})

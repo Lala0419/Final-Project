871de268a1083931b084e9cafd483bf792f3e053
MrMoon127 <sasha.andia@gmail.com>
2023-11-17 17:38:16 -0800

    finished crude version of footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,20 +7,35 @@ function Footer() {
   return (
     <footer>
       <div className="social-btns">
-
-      </div>
-
-      <div className="body">
-
-      </div>
-
-      <div className="social-btns">
         <button className='social-btn'>
           <FaFacebookF />
         </button>
         <button className='social-btn'>
           <FaRegEnvelope />
         </button>
+      </div>
+
+      <div className="footer__body">
+        <h1 className="footer__header">
+          Subscribe to our Seasonal Newsletter
+        </h1>
+        <p className="footer__p">
+          Sign up with your email address to receive deals and discounts an our services.
+        </p>
+
+        <form method="post" action="none" className="newsletter__form">
+          <textarea className="newsletter__textarea" name="newsletter-text" placeholder="Email Address"></textarea>
+          <input type="submit" value="Sign Up" className="newsletter__input" onClick={(event) => event.preventDefault()} />
+        </form>
+
+        <p className="message">
+          We respect your privacy.
+        </p>
+      </div>
+
+      <div className="nav-btns">
+      <button className='nav-btn'>Phone Number</button>
+      <button className='nav-btn'>Free Estimate</button>
       </div>
     </footer>
   );

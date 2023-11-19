6bf57b4df47c32bc9e3eede505fd4639759a0c16
Nanaya <nanayamiki@gmail.com>
2023-11-19 09:29:02 -0800

    create Reviews component

---
+++ frontend/src/components/reviews/Reviews.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Reviews.scss";
+
+function Reviews() {
+	return (
+		<>
+			<div className="reviews">
+				<div className="reviews_top">
+					<h2 className="reviews_top-header">OUR MOTIVATION</h2>
+					<p class="header_top-subtitle">
+						Your feedbacks motivate us to improve our servies!
+					</p>
+				</div>
+				<div className="reviews_bottom"></div>
+			</div>
+		</>
+	);
+}
+
+export default Reviews;

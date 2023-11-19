708fb5359f6b5413339d54e8f5f843919857e845
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-11-19 15:03:45 -0800

    create estimate form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
-import Header from "./components/header/Header";
+import Estimate from './components/estimate/Estimate';
+import Header from './components/header/Header';
 
 function App() {
-	return <Header />;
+  return (
+    <>
+      <Header />
+      <Estimate />
+    </>
+  );
 }
 
 export default App;

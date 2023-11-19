4b5fb287b8130f2f183cba6070b22bd6545ddde2
Nanaya <nanayamiki@gmail.com>
2023-11-19 08:06:38 -0800

    update ServiceCard component

--- frontend/src/components/serviceCard/ServiceCard.jsx
+++ frontend/src/components/serviceCard/ServiceCard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./ServiceCard.scss";
 
-function ServiceCard() {
-	return <div>ServiceCard</div>;
+function ServiceCard(props) {
+	return (
+		<div className="card">
+			<h2 className="card_title">{props.title}</h2>
+			<p className="card_subtitle">{props.subtitle}</p>
+			<p className="card_desc">{props.desc}</p>
+			<div className="card_imgs">
+				<div className="card_imgs-item">
+					<span className="card_imgs-item--caption">Before</span>
+					<img
+						className="card_imgs-item--img"
+						src={props.img_before_url}
+						alt="before"
+					/>
+				</div>
+				<div className="card_imgs-item">
+					<span className="card_imgs-item--caption">After</span>
+					<img
+						className="card_imgs-item--img"
+						src={props.img_after_url}
+						alt="after"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ServiceCard;

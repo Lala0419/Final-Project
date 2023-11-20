31d02b11deae7e3a96868c4255dfe55db233203a
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-11-19 18:28:20 -0800

    add estimate page to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import EstimatePage from './pages/estimatePage/EstimatePage';
 import Home from './pages/home/Home';
 
 function App() {
-  return <Home />;
+  return (<>
+    <Home />
+    <EstimatePage />
+  </>);
 }
 
 export default App;

21f0d6ed328c755c59fd443bce0ea5d83fd32555
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-11-20 22:10:48 -0800

    fix class name

--- frontend/src/components/estimate/Estimate.jsx
+++ frontend/src/components/estimate/Estimate.jsx
@@ -10,86 +10,86 @@ const Estimate = () => {
           <label for="name" className="estimate_form_label">
             Name <span className="estimate_form_label-muted">(required)</span>
           </label>
-          <div class="form-row">
-            <div class="form-group col ">
+          <div className="form-row">
+            <div className="form-group col ">
               <label for="name" className="estimate_form_label">
                 First Name
               </label>
-              <input type="text" class="form-control form-control-lg" />
+              <input type="text" className="form-control form-control-lg" />
             </div>
-            <div class="form-group col">
+            <div className="form-group col">
               <label for="name" className="estimate_form_label">
                 Last Name
               </label>
-              <input type="text" class="form-control form-control-lg" />
+              <input type="text" className="form-control form-control-lg" />
             </div>
           </div>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="phone" className="estimate_form_label">
             Phone <span className="estimate_form_label-muted">(required)</span>
           </label>
-          <input type="number" class="form-control form-control-lg" />
+          <input type="number" className="form-control form-control-lg" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="house-address" className="estimate_form_label">
             House Address <span className="estimate_form_label-muted">(required)</span>
           </label>
-          <input type="text" class="form-control form-control-lg" />
+          <input type="text" className="form-control form-control-lg" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="email" className="estimate_form_label">
             Email Address <span className="estimate_form_label-muted">(required)</span>
           </label>
-          <input type="email" class="form-control form-control-lg" />
+          <input type="email" className="form-control form-control-lg" />
         </div>
 
         <p className="estimate_form_label_bold">Select which service(s) you want:</p>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_1" />
-          <label class="form-check-label" for="service_1">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_1" />
+          <label className="form-check-label" for="service_1">
             Window Washing
           </label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_2" />
-          <label class="form-check-label" for="service_2">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_2" />
+          <label className="form-check-label" for="service_2">
             Siding Soft Wash
           </label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_3" />
-          <label class="form-check-label" for="service_3">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_3" />
+          <label className="form-check-label" for="service_3">
             Gutter Cleaning
           </label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_4" />
-          <label class="form-check-label" for="service_4">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_4" />
+          <label className="form-check-label" for="service_4">
             Fascia/Trim/Soffit Cleaning
           </label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_5" />
-          <label class="form-check-label" for="service_5">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_5" />
+          <label className="form-check-label" for="service_5">
             Roof Cleaning
           </label>
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="" id="service_6" />
-          <label class="form-check-label" for="service_6">
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="checkbox" value="" id="service_6" />
+          <label className="form-check-label" for="service_6">
             Moss Removal and Treatment
           </label>
         </div>
 
-        <div class="form-group">
+        <div className="form-group">
           <label for="name" className="estimate_form_label">
             Any specific requests?<span className="estimate_form_label-muted">(required)</span>
           </label>
-          <textarea type="text" class="form-control text-area"></textarea>
+          <textarea type="text" className="form-control text-area"></textarea>
         </div>
 
-        <button type="submit" class="btn btn-dark btn-lg">
+        <button type="submit" className="btn btn-dark btn-lg">
           Submit
         </button>
       </form>

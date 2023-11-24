297920b619a7b3d0a16f44882b780cadbea3ec06
Nanaya <nanayamiki@gmail.com>
2023-11-23 16:06:23 -0800

    wrap the App component with snackbarProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,30 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/Home';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home";
 // import About from "./pages/about/About";
 // import Service from "./pages/servicePage/Service";
-import EstimatePage from './pages/estimatePage/EstimatePage';
-import Navigation from './components/navigation/Navigation';
-import Footer from './components/footer/Footer';
+import EstimatePage from "./pages/estimatePage/EstimatePage";
+import Navigation from "./components/navigation/Navigation";
+import Footer from "./components/footer/Footer";
+import SnackbarProvider from "react-simple-snackbar";
 
 function App() {
-  return (
-    <>
-      {/* <Navigation /> */}
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/service" element={<Service />} /> */}
-        <Route path="/free-estimate" element={<EstimatePage />} />
-        {/* <Route path="/admin" element={<Admin />} /> */}
-      </Routes>
+	return (
+		<>
+			{/* <Navigation /> */}
+			<SnackbarProvider>
+				<Routes>
+					<Route path="/" element={<Navigate to="/home" />} />
+					<Route path="/home" element={<Home />} />
+					{/* <Route path="/about" element={<About />} /> */}
+					{/* <Route path="/service" element={<Service />} /> */}
+					<Route path="/free-estimate" element={<EstimatePage />} />
+					{/* <Route path="/admin" element={<Admin />} /> */}
+				</Routes>
+			</SnackbarProvider>
 
-      {/* <Footer /> */}
-    </>
-  );
+			{/* <Footer /> */}
+		</>
+	);
 }
 
 export default App;

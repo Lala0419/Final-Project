0c5bb3976d56b918cf919057e8006191fd090c5c
Nanaya <nanayamiki@gmail.com>
2023-12-01 14:43:15 -0800

    add partial styling for Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,31 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home/Home';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home";
 import AboutPage from "./pages/about/About";
 // import Service from "./pages/servicePage/Service";
-import EstimatePage from './pages/estimatePage/EstimatePage';
-import Navigation from './components/navigation/Navigation';
-import Footer from './components/footer/Footer';
-import SnackbarProvider from 'react-simple-snackbar';
-import Admin from './pages/admin/Admin';
+import EstimatePage from "./pages/estimatePage/EstimatePage";
+import Navigation from "./components/navigation/Navigation";
+import Footer from "./components/footer/Footer";
+import SnackbarProvider from "react-simple-snackbar";
+import Admin from "./pages/admin/Admin";
 
 function App() {
-  return (
-    <>
-      <SnackbarProvider>
-        {/* <Navigation /> */}
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          {/* <Route path="/service" element={<Service />} /> */}
-          <Route path="/free-estimate" element={<EstimatePage />} />
+	return (
+		<>
+			<SnackbarProvider>
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Navigate to="/home" />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/about" element={<AboutPage />} />
+					{/* <Route path="/service" element={<Service />} /> */}
+					<Route path="/free-estimate" element={<EstimatePage />} />
 
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-        <Footer />
-      </SnackbarProvider>
-    </>
-  );
+					<Route path="/admin" element={<Admin />} />
+				</Routes>
+				<Footer />
+			</SnackbarProvider>
+		</>
+	);
 }
 
 export default App;
--- frontend/src/components/navigation/Navigation.jsx
+++ frontend/src/components/navigation/Navigation.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import './Navigation.scss'
-
+import "./Navigation.scss";
 
 function Navigation() {
-  const logo = "/facebookcover.png"
+	const logo = "/facebookcover.png";
 
-  return (
+	return (
+		<>
+			<div className="nav-mobile">
+				<div className="nav-mobile-left">
+					<img
+						className="nav-mobile-img"
+						src={logo}
+						alt="Logo for Property Brothers"
+					/>
+				</div>
 
-    <div className='nav'>
+				<div className="nav-mobile-right"></div>
+			</div>
 
-      <div className='nav-left'>
-        <img className='nav-img' src={logo} alt='Logo for Property Brothers' />
-      </div>
+			<div className="nav">
+				<div className="nav-left">
+					<img
+						className="nav-img"
+						src={logo}
+						alt="Logo for Property Brothers"
+					/>
+				</div>
 
-      <div className='nav-right'>
-        <button className='nav-button'>Phone Number</button>
-        <button className='nav-button'>Home</button>
-        <button className='nav-button'>About</button>
-        <button className='nav-button'>Services</button>
-        <button className='nav-button'>Free Estimate</button>
-      </div>
-
-    </div>
-    
-  );
+				<div className="nav-right">
+					<button className="nav-button">Phone Number</button>
+					<button className="nav-button">Home</button>
+					<button className="nav-button">About</button>
+					<button className="nav-button">Services</button>
+					<button className="nav-button">Free Estimate</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Navigation;

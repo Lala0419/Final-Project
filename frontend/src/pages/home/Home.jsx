2804833e77befbfd0ee4080d7e9a147f8e771166
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-12-03 14:42:42 -0800

    change admin new estimate notification

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Header from "../../components/header/Header";
-import ServiceMain from "../../components/serviceMain/ServiceMain";
-import Reviews from "../../components/reviews/Reviews";
-import FacebookMessenger from "../../components/facebookMessenger/FacebookMessenger";
+import React from 'react';
+import Header from '../../components/header/Header';
+import ServiceMain from '../../components/serviceMain/ServiceMain';
+import Reviews from '../../components/reviews/Reviews';
+import FacebookMessenger from '../../components/facebookMessenger/FacebookMessenger';
 
 function Home() {
-	return (
-		<>
-			{/* <Header />
-			<ServiceMain />
-			<Reviews /> */}
-			<FacebookMessenger />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <ServiceMain />
+      <Reviews />
+      <FacebookMessenger />
+    </>
+  );
 }
 
 export default Home;

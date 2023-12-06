dd2b1f615257ea7ce978be9e1d4cbefafec8da79
Nanaya <nanayamiki@gmail.com>
2023-12-06 14:53:36 -0800

    adjust the styling on the home  page

--- frontend/src/components/serviceCard/ServiceCard.jsx
+++ frontend/src/components/serviceCard/ServiceCard.jsx
@@ -3,23 +3,23 @@ import "./ServiceCard.scss";
 
 function ServiceCard(props) {
 	return (
-		<div className="card">
-			<h2 className="card_title">{props.title}</h2>
-			<p className="card_subtitle">{props.subtitle}</p>
-			{/* <p className="card_desc">{props.desc}</p> */}
-			<div className="card_imgs">
-				<div className="card_imgs-item">
-					<span className="card_imgs-item--caption">Before</span>
+		<div className="service-card">
+			<h2 className="service-card_title">{props.title}</h2>
+			<p className="service-card_subtitle">{props.subtitle}</p>
+			{/* <p className="service-card_desc">{props.desc}</p> */}
+			<div className="service-card_imgs">
+				<div className="service-card_imgs-item">
+					<span className="service-card_imgs-item--caption">Before</span>
 					<img
-						className="card_imgs-item--img"
+						className="service-card_imgs-item--img"
 						src={props.img_before_url}
 						alt="before"
 					/>
 				</div>
-				<div className="card_imgs-item">
-					<span className="card_imgs-item--caption">After</span>
+				<div className="service-card_imgs-item">
+					<span className="service-card_imgs-item--caption">After</span>
 					<img
-						className="card_imgs-item--img"
+						className="service-card_imgs-item--img"
 						src={props.img_after_url}
 						alt="after"
 					/>

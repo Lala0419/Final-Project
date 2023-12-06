d2b6380fa50c820221c62fcbf07e6eca919ada80
MrMoon127 <sasha.andia@gmail.com>
2023-12-05 19:30:49 -0800

    added states

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Admin.scss";
 import AdminEmail from "../../components/admin-email/admin-email";
 import AdminEstimateList from "../../components/admin-estimate/AdminEstimateList";
 
 function Admin() {
+
+  const [adminDisplay, setAdminDisplay] = useState('email');
+
 
   return (
     <div>

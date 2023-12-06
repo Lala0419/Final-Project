e43b7ae84fb9ce702bde80cbc1d1f6170beca7aa
Nanaya <nanayamiki@gmail.com>
2023-12-05 21:33:51 -0800

    adjust the styling on the admin page

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -4,18 +4,20 @@ import AdminEmail from "../../components/admin-email/admin-email";
 import AdminEstimateList from "../../components/admin-estimate/AdminEstimateList";
 
 function Admin() {
+	const [adminDisplay, setAdminDisplay] = useState("estimate");
 
-  const [adminDisplay, setAdminDisplay] = useState('email');
+	const switchAdmin = () =>
+		setAdminDisplay(adminDisplay === "email" ? "estimate" : "email");
 
-  const switchAdmin = () => setAdminDisplay(adminDisplay === 'email' ? 'estimate' : 'email');
-
-  return (
-    <div>
-      <button className="button" onClick={switchAdmin} >Show {adminDisplay === 'email' ? 'Estimate' : 'Email'} List</button>
-      {adminDisplay === 'email' && <AdminEmail />}
-      {adminDisplay === 'estimate' && <AdminEstimateList />}
-    </div>
-  )
+	return (
+		<div>
+			<span className="button" onClick={switchAdmin}>
+				Show {adminDisplay === "email" ? "Estimate" : "Email"} List
+			</span>
+			{adminDisplay === "email" && <AdminEmail />}
+			{adminDisplay === "estimate" && <AdminEstimateList />}
+		</div>
+	);
 }
 
 export default Admin;
